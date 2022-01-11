e27f0669e397f2bb15c4c781e5a4f849ed9e1c09
Asef <50338626+Asef00@users.noreply.github.com>
2022-01-11 18:10:46 +0330

    [#1] a11y-dialog (bug)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,35 @@
 /* eslint-env browser */
-(function() {
+(function () {
   'use strict';
-  document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function () {
     // Your custom JavaScript goes here
+    // var dialogEl = document.getElementById('success-modal');
+    // var dialog = new A11yDialog(dialogEl);
+
+    const element = document.getElementById('your-dialog-id')
+
+    const dialog = new A11yDialog(element)
+
+    // var form = document.querySelector('.c-footer__form');
+    // form.addEventListener('submit', function (event) {
+    //   event.preventDefault();
+    //   var FD = new FormData(form);
+    //   var error = false;
+
+    //   // Name Field
+    //   if (!FD.get('name')) {
+    //     error = true;
+    //   }
+
+    //   // Everything is fine
+    //   if (!error) {
+    //     // Show the dialog
+    //     dialog.show();
+    //   }
+    // });
   });
+
+
 })();
+
+
