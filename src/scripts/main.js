b5b0023d8b3364f24764ded21e6ee5f7d1dd4a87
Asef <50338626+Asef00@users.noreply.github.com>
2022-01-11 19:43:14 +0330

    [#1] subscribe modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,27 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function () {
     // Your custom JavaScript goes here
-    // var dialogEl = document.getElementById('success-modal');
-    // var dialog = new A11yDialog(dialogEl);
 
-    const element = document.getElementById('your-dialog-id')
+    var dialog = new A11yDialog(document.getElementById('success-modal'));
 
-    const dialog = new A11yDialog(element)
+    var form = document.querySelector('.c-footer__form');
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      var FD = new FormData(form);
+      var error = false;
 
-    // var form = document.querySelector('.c-footer__form');
-    // form.addEventListener('submit', function (event) {
-    //   event.preventDefault();
-    //   var FD = new FormData(form);
-    //   var error = false;
+      // Name Field
+      if (!FD.get('name') || !FD.get('email')) {
+        error = true;
+      }
 
-    //   // Name Field
-    //   if (!FD.get('name')) {
-    //     error = true;
-    //   }
-
-    //   // Everything is fine
-    //   if (!error) {
-    //     // Show the dialog
-    //     dialog.show();
-    //   }
-    // });
+      // Everything is fine
+      if (!error) {
+        // Show the dialog
+        dialog.show();
+      }
+    });
   });
-
-
 })();
 
 
